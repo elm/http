28a580a2c008e1c81a2302395a5aa53a147f7848
Evan Czaplicki <evancz@users.noreply.github.com>
2016-10-05 00:08:38 -0700

    Draft the Http.RateLimit API
    Need to outline all the different strategies before implementing stuff

--- src/Http/RateLimit.elm
+++ src/Http/RateLimit.elm
@@ -1,48 +1,88 @@
 effect module Http.RateLimit where { subscription = MySub } exposing
   ( rateLimit
+  , Msg(..)
   , Strategy
   )
+
+
+{-| Make HTTP requests, but rate limit them. This way you con reduce the load
+on your servers without degrading the experience for users too much.
+
+# Rate Limiting
+@docs rateLimit, Msg
+
+# Strategies
+@docs Strategy
+-}
 
 
 import Dict
 import Http
 import Http.Internal
 import Platform
+import Process
 import Task exposing (Task)
 import Time exposing (Time)
-
-
-
---
-
-
-type Tracker data =
-  Tracker String Strategy (Buffer data)
-
-
-type Buffer data
-  = NoResults
-  | Stale data
-  | Fresh data
-
-
-type Response data = Fail Http.Error | Done data
 
 
 
 -- RATE LIMIT
 
 
-track : Tracker data -> Request data -> Sub data
-track (Tracker id strategy _) (Request request) =
-  MySub id strategy request
+{-| Subscribe to the results of rate-limited HTTP requests. The arguments are:
+
+  1. A unique ID that groups requests. Say you have three independent sources
+  of HTTP requests, and you want each source rate-limited independently. You
+  want three `rateLimit` subscriptions, each with a different ID.
+
+  2. A strategy for rate-limiting requests. More on that later!
+
+  3. A way to turn rate-limited results into messages for your `update`.
+
+  4. The latest `Request` you want to run. It will not get called a bunch of
+  times. A `rateLimit` subscription looks at the method, URL, headers, body,
+  and other settings to figure out if it has already been sent.
+
+Together, this means you can always derive your `rateLimit` subscription
+directly from your `Model`. Figure out the `Request` you care about right now
+and the messages will come in.
+
+**Note:** A good trick for creating unique IDs is to make it the name of the
+module plus some additional description of what it is for.
+-}
+rateLimit : String -> Strategy -> (Msg data -> msg) -> Http.Request data -> Sub msg
+rateLimit id strategy toMessage (Http.Internal.Request request) =
+  subscription <| MySub <|
+    { id = id
+    , strategy = strategy
+    , request = Http.Internal.map (toMessage << Success) request
+    , onError = toMessage << Failure
+    }
+
+
+
+-- MESSAGES
+
+
+{-| When you subscribe to the results of a rate-limited request, there are
+three kinds of messages you can get back. When a request completes, you will
+get a `Success` or `Failure` message. When that result becomes stale because
+new requests have come in, you will get a `Waiting` message.
+-}
+type Msg data
+  = Waiting
+  | Failure Http.Error
+  | Success data
 
 
 
 -- STRATEGIES
 
 
-type Strategy = Strategy
+{-| There are a couple ways to rate limit HTTP requests. Only call once
+things have settled down? Never delay longer than 300 milliseconds? Etc.
+-}
+type Strategy = TODO
 
 
 
@@ -50,12 +90,22 @@ type Strategy = Strategy
 
 
 type MySub msg =
-  MySub String Time (Http.Internal.RawRequest msg)
+  MySub
+    { id : String
+    , strategy : Strategy
+    , request : Http.Internal.RawRequest msg
+    , onError : Http.Error -> msg
+    }
 
 
 subMap : (a -> b) -> MySub a -> MySub b
-subMap func (MySub id time request) =
-  MySub id time (Http.Internal.map func request)
+subMap func (MySub { id, strategy, request, onError }) =
+  MySub
+    { id = id
+    , strategy = strategy
+    , request = Http.Internal.map func request
+    , onError = func << onError
+    }
 
 
 
@@ -81,75 +131,9 @@ init =
 
 onEffects : Platform.Router msg Never -> List (MySub msg) -> State -> Task Never State
 onEffects router subs state =
-  let
-    subDict =
-      collectSubs subs
-
-    leftStep id {latest} (dead, ongoing, new) =
-      ( Process.kill latest :: dead
-      , ongoing
-      , new
-      )
-
-    bothStep id {lastTask, } _ (dead, ongoing, new) =
-      ( dead
-      , Dict.insert id process ongoing
-      , new
-      )
-
-    rightStep id trackedRequest (dead, ongoing, new) =
-      ( dead
-      , ongoing
-      , (id, trackedRequest) :: new
-      )
-
-    (dead, ongoing, new) =
-      Dict.merge leftStep bothStep rightStep state subDict ([], Dict.empty, [])
-  in
-    Task.sequence dead
-      |> Task.andThen (\_ -> spawnRequests router new ongoing)
-
-
-spawnRequests : Router msg Never -> List (String, TrackedRequest msg) -> State -> Task Never State
-spawnRequests router trackedRequests state =
-  case trackedRequests of
-    [] ->
-      Task.succeed state
-
-    (id, trackedRequest) :: others ->
-      Process.spawn (toTask router trackedRequest)
-        |> Task.andThen (\process -> spawnRequests router others (Dict.insert id process state))
-
-
-toTask : Router msg Never -> TrackedRequest msg -> Task Never ()
-toTask router { request, toProgress, toError } =
-  Native.Http.toTask request (Just toProgress)
-    |> Task.andThen (Platform.sendToApp router)
-    |> Task.onError (Platform.sendToApp router << toError)
-
-
-
--- COLLECT SUBS AS DICT
-
-
-type alias SubDict msg =
-  Dict.Dict String { cooldown : Time, request : Http.Internal.RawRequest msg }
-
-
-collectSubs : List (MySub msg) -> SubDict msg
-collectSubs subs =
-  List.foldl addSub Dict.empty subs
-
-
-addSub : MySub msg -> SubDict msg -> SubDict msg
-addSub (MySub id cooldown request) subDict =
-  Dict.insert id { cooldown = cooldown, request = request } subDict
-
-
-
--- SELF MESSAGES
+  Debug.crash "TODO"
 
 
 onSelfMsg : Platform.Router msg Never -> Never -> State -> Task Never State
 onSelfMsg router _ state =
-  Task.succeed state
+  Debug.crash "TODO"
